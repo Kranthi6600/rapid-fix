73c07525ecfac3d9e00616d4ce050e607a7868b7
kranthi <sriramkranthikumar7672@gmail.com>
2026-05-09 06:23:04 +0530

    favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,6 +7,11 @@ export const metadata = {
   title: "Rapid Fix - Car Repair & Auto Services NEXT Js Template",
   description:
     "Rapid Fix is a clean and modern React Template suitable for any type of Auto Servicing, Car Repair & Maintenance website. It is created for automobile servicing providers who offer car wash, car service, routine maintenance, truck service, etc. This HTML template can be used for car servicing, car repairing, car wash, auto shop, mechanic shop, batteries shop, tire / wheel shop, and multipurpose businesses. We have used comment on codes and also decorated the codes beautifully so one can find it very easy to customize.",
+  icons: {
+    icon: "/assets/img/favicons/favicon.jpeg",
+    shortcut: "/assets/img/favicons/favicon.jpeg",
+    apple: "/assets/img/favicons/favicon.jpeg",
+  },
 };
 
 export default function RootLayout({ children }) {
