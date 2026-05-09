0a30fd0418d051959dffa57a7ab492e80c17c81c
kranthi <sriramkranthikumar7672@gmail.com>
2026-05-09 06:28:34 +0530

    favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,9 +8,9 @@ export const metadata = {
   description:
     "Rapid Fix is a clean and modern React Template suitable for any type of Auto Servicing, Car Repair & Maintenance website. It is created for automobile servicing providers who offer car wash, car service, routine maintenance, truck service, etc. This HTML template can be used for car servicing, car repairing, car wash, auto shop, mechanic shop, batteries shop, tire / wheel shop, and multipurpose businesses. We have used comment on codes and also decorated the codes beautifully so one can find it very easy to customize.",
   icons: {
-    icon: "/assets/img/favicons/favicon.jpeg",
-    shortcut: "/assets/img/favicons/favicon.jpeg",
-    apple: "/assets/img/favicons/favicon.jpeg",
+    icon: "/assets/img/favicons/favicon.png",
+    shortcut: "/assets/img/favicons/favicon.png",
+    apple: "/assets/img/favicons/favicon.png",
   },
 };
 
