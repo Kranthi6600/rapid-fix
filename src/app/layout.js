6f4ad0d0b6f122c25190a052ce50d7a1d54bd25e
kranthi kumar <kranthichintu6600@gmail.com>
2026-07-17 23:24:53 +0530

    fix: use next/script for Meta Pixel and GTM for proper execution

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import BootstrapInit from "@/helper/BootstrapInit";
 import Animation from "@/helper/Animation";
 import { ServicesProvider } from "@/context/ServicesContext";
@@ -21,18 +22,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TXJFDMFH');`,
-          }}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','2064492734143343');fbq('track','PageView');`,
-          }}
-        />
-      </head>
       <body suppressHydrationWarning={true}>
         <noscript
           dangerouslySetInnerHTML={{
@@ -44,6 +33,12 @@ export default function RootLayout({ children }) {
             __html: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=2064492734143343&ev=PageView&noscript=1" />`,
           }}
         />
+        <Script id="gtm-script" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TXJFDMFH');`}
+        </Script>
+        <Script id="meta-pixel-script" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','2064492734143343');fbq('track','PageView');`}
+        </Script>
         <BootstrapInit />
         <Animation />
         <ServicesProvider>
